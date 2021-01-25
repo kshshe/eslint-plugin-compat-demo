9d6bad82253b396fc9c44a89d29e52c9905f3a56
Алексей Лауд <a.laud@hh.ru>
2021-01-25 15:43:40 +0300

    fail demo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-// This does not throw because browserslist is overriden for this subdirectory
-// This subdirectory only throws APIs unsupported in chrome@70
-Object.values({
-  foo: 'bar'
-});
+new Promise(() => {})
